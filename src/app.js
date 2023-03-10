6378533194c7b5521d13c6df24d1da012596dac4
Zachery  Wilson <zilson1987@gmail.com>
2023-03-10 10:19:54 -0600

    changed entry point to index.html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
-const socialIconLinks = document.querySelector('.socialIcons').children;
+const socialIconLinksChildren = document.querySelector('.socialIcons').children;
 
-[...socialIconLinks].forEach((icon) => {
+[...socialIconLinksChildren].forEach((icon) => {
   icon.addEventListener('mouseover', () => {
     icon.classList.toggle('socialIcons_icon--hover');
   });
